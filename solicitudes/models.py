549b303c2c33c14ce1bee010dd10a2a2eae96c2b
Jorge Martín <jormartin77@gmail.com>
2019-09-12 22:18:40 -0500

    Se crea modelo de solicitud base

--- solicitudes/models.py
+++ solicitudes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Solicitud(models.Model):
+    nombre = models.TextField()
+    script = models.TextField()
+    creado_por = models.ForeignKey(User, on_delete=models.CASCADE())
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
